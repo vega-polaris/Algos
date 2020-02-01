b7cafe9493b6631d7ce06f54d27acf2f9d36654d
Featherweight <vais.emma@gmail.com>
2020-02-01 14:50:20 -0500

    solved Island Count

--- pramp/sentenceReverse.js
+++ pramp/sentenceReverse.js
@@ -1,50 +1,50 @@
-arr = [
-    'p',
-    'e',
-    'r',
-    'f',
-    'e',
-    'c',
-    't',
-    '  ',
-    'm',
-    'a',
-    'k',
-    'e',
-    's',
-    '  ',
-    'p',
-    'r',
-    'a',
-    'c',
-    't',
-    'i',
-    'c',
-    'e',
-]
+const arr = [
+  "p",
+  "e",
+  "r",
+  "f",
+  "e",
+  "c",
+  "t",
+  "  ",
+  "m",
+  "a",
+  "k",
+  "e",
+  "s",
+  "  ",
+  "p",
+  "r",
+  "a",
+  "c",
+  "t",
+  "i",
+  "c",
+  "e"
+];
 
 function reverseWords(arr) {
-    // loop through input array, putting each letter in a string
-    let tempArray = []
-    let reversedArray = []
-    arr.forEach((letter, idx) => {
-        // when the letter is ' ', unshift the string into an array and start a new string
-        if (letter === '  ') {
-            // create this array with all the elements in the other two arrays
-            // check for first word in array
-            if (reversedArray[0]) {
-                tempArray.push(letter)
-            }
-            reversedArray = [...tempArray, ...reversedArray]
-            tempArray = []
-        } else if (idx === arr.length - 1) {
-            tempArray.push(letter, '  ')
-        } else {
-            tempArray.push(letter)
-        }
-    })
+  // loop through input array, putting each letter in a string
+  let tempArray = [];
+  let reversedArray = [];
+  arr.forEach((letter, idx) => {
+    // when the letter is ' ', unshift the string into an array and start a new string
+    if (letter === "  ") {
+      // create this array with all the elements in the other two arrays
+      // check for first word in array
+      if (reversedArray[0]) {
+        tempArray.push(letter);
+      }
+      reversedArray = [...tempArray, ...reversedArray];
+      tempArray = [];
+    } else if (idx === arr.length - 1) {
+      tempArray.push(letter, "  ");
+    } else {
+      tempArray.push(letter);
+    }
+  });
 
-    return [...tempArray, ...reversedArray]
+  return [...tempArray, ...reversedArray];
 }
 
-reverseWords(arr)
+reverseWords(arr);
