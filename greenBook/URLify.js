ad6bcd9a0d3e2d949911455577e34f9bd78d30a3
Featherweight <vais.emma@gmail.com>
2020-02-14 14:44:17 -0500

    solved URLify

---
+++ greenBook/URLify.js
@@ -0,0 +1,21 @@
+const URLify = function(string, length) {
+  // create new string
+  let urlString = '';
+  // use a flag.
+  let postSpace = false;
+  // loop through string, storing every letter
+  for (let i = 0; i < length; i++) {
+    //Switch flag to true when hitting a space. When hitting a letter and the flag is true, join a %20 before the letter.
+    if (string[i] === ' ') {
+      postSpace = true;
+    } else {
+      if (postSpace) {
+        urlString += '%20' + string[i];
+        postSpace = false;
+      } else {
+        urlString += string[i];
+      }
+    }
+  }
+  return urlString;
+};
