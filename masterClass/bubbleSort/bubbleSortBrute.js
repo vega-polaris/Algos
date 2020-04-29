d7775cb3657bf9a73fa7ccd33678d267a250f6d9
Featherweight <vais.emma@gmail.com>
2020-04-29 16:51:29 -0400

    brute force solution to bubble sort

---
+++ masterClass/bubbleSort/bubbleSortBrute.js
@@ -0,0 +1,16 @@
+bubbleSort = (arr) => {
+  for (let i = arr.length; i > -1; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+    }
+  }
+  return arr;
+};
+
+const swap = function (arr, firstIdx, secondIdx) {
+  let temp = arr[firstIdx];
+  arr[firstIdx] = arr[secondIdx];
+  arr[secondIdx] = temp;
+};
+
+bubbleSort([2, 2, 2, 1, 5, 12213132, 34343, 556, 123]);
