0064a06949d04c3e0497470af59c13553b58f57b
featherweightdev <vais.emma@gmail.com>
2020-05-21 21:19:51 -0400

    solved quicksort my way

--- masterClass/intermediateSorts/quickSort.js
+++ masterClass/intermediateSorts/quickSort.js
@@ -1,16 +1,10 @@
-/* pivot helper
-given an array, this func designates an element as a pivot and rearranges the elements so that all elements smaller than pivot are on the left, all the bigger are on the right. The specific order on either side doesn't matter.
-This should be done IN PLACE, and returns the new index of the pivot.
-How do we pick pivot? We would like to choose the median value of the data, but since we don't know the contents of the array, we have to have other solutions. For this pseudo-code, we'll choose the first element as a pivot. */
-
 //  Accept three args: array, start index, end index.
 const pivotHelper = function (array, startIdx = 0, endIdx = array.length) {
   //Store current pivot in a var (which will keep track of where the pivot should end up).
-  let swapIdx = 0;
+  let swapIdx = startIdx;
   let pivotElement = array[startIdx];
-  let swapped = false;
   // Loop through array (from start to end).
-  for (let i = startIdx + 1; i < endIdx; i++) {
+  for (let i = startIdx + 1; i < endIdx + 1; i++) {
     let element = array[i];
     // If pivot is greater than current element, increment the pivot index variable and swap current element with element at pivot index
     if (pivotElement > element) {
@@ -18,7 +12,6 @@ const pivotHelper = function (array, startIdx = 0, endIdx = array.length) {
       swap(array, swapIdx, i);
     }
   }
-
   swap(array, swapIdx, startIdx);
   // return new pivot index
   return swapIdx;
@@ -29,7 +22,12 @@ const swap = function (array, idx1, idx2) {
   let temp = array[idx1];
   array[idx1] = array[idx2];
   array[idx2] = temp;
-  console.log({ arrayFromSwap: array });
 };
 
-pivotHelper([4, 1, -3, 70, 34, 9, 2]);
+const quickSort = function (array, startIdx = 0, endIdx = array.length - 1) {
+  if (startIdx >= endIdx) return array;
+  let swapIdx = pivotHelper(array, startIdx, endIdx);
+  quickSort(array, startIdx, swapIdx);
+  quickSort(array, swapIdx + 1, endIdx);
+  return array;
+};
