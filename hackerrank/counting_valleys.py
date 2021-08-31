d23b2f1a36303b58dd0364331dab49d66fd85046
featherweightdev <vais.emma@gmail.com>
2021-08-31 13:57:01 -0400

    added description to counting valleys

--- hackerrank/counting_valleys.py
+++ hackerrank/counting_valleys.py
@@ -1,5 +1,4 @@
-"""
-An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+"""An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly steps, for every step it was noted if it was an uphill, D, or a downhill, D, step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
 
 A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
 A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
