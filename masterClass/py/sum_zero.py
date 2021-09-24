d760acebb9a47d323ab84d5748085bf669a95bb8
Featherweight <vais.emma@gmail.com>
2021-09-24 17:47:13 +0300

    refactored sum zero

--- masterClass/py/sum_zero.py
+++ masterClass/py/sum_zero.py
@@ -7,18 +7,17 @@ def sum_zero(arr):
         return None
     # two pointers - left and right
     left = 0
-    right = 1
-    while left > -1 and right < len(arr):
+    right = len(arr) - 1
+    while left < right:
         # if they're pointing at complimentary numbers, return them
         cur_sum = arr[left] + arr[right]
         if cur_sum == 0:
             return [arr[left], arr[right]]
         # if not, check if you need to go one left or one right
         elif cur_sum > 0:
-            left -= 1
+            right -= 1
         else:
-            right += 1
-    
+            left += 1
     # if there's no more right or left to go, return None
     return None
 
