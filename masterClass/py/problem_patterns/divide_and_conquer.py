f1202bee5e8f20bbe2bc635172bf12f1111973f7
Featherweight <vais.emma@gmail.com>
2021-10-13 20:24:21 -0400

    solved divide and conquer

---
+++ masterClass/py/problem_patterns/divide_and_conquer.py
@@ -0,0 +1,30 @@
+import math 
+
+def search(array, n):
+    """
+    Given a sorted array of ints, return the index at which n is found.
+    If it is not found, return -1.
+    """
+    # middle = math.floor(len(array) / 2)
+    left = 0
+    right = len(array)
+    while left < right:
+        middle = math.floor((right + left) / 2)
+        if array[middle] > n:
+            right = middle - 1
+        elif array[middle] < n:
+            left = middle + 1
+        else:
+            return middle
+    if right == left:
+        return right
+    return -1
+
+# print("weellllll")
+print(search([1, 2, 3, 4, 5, 6], 4)) # 3
+print(search([1, 2, 3, 4, 5, 6], 6)) # 5
+print(search([1, 2, 3, 4, 5, 6], 2)) # 1
+print(search([1, 2, 3, 4, 5, 6], 11)) # -1
+print(search([1, 2, 3, 4, 5, 6], 1)) # 0
+
+
